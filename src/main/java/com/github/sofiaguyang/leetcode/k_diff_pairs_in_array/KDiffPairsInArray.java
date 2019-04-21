b1d2fb33b0ecb91db7aa5cae8e3aef11335a672a
Sofia <sofiaguyang@users.noreply.github.com>
2019-04-22 00:09:47 +0800

    Implement k-diff pairs in array solution

---
+++ src/main/java/com/github/sofiaguyang/leetcode/k_diff_pairs_in_array/KDiffPairsInArray.java
@@ -0,0 +1,63 @@
+package com.github.sofiaguyang.leetcode.k_diff_pairs_in_array;
+
+import java.util.*;
+
+public class KDiffPairsInArray {
+    public int findPairs(int[] nums, int k) {
+        if(k < 0) // because absolute difference can never be negative
+            return 0;
+
+        Map<Integer, Integer> num2Count = new HashMap<>();
+        for (int num : nums) {
+            num2Count.computeIfPresent(num, (key, value) -> value + 1);
+            num2Count.putIfAbsent(num, 1);
+        }
+
+        Set<Pair> pairSet = new HashSet<>();
+        for (int num : nums) {
+            int partner = num + k;
+            int partnerCount = num2Count.getOrDefault(partner, 0);
+            if (partner == num && partnerCount > 1)
+                pairSet.add(new Pair(num, partner));
+            else if (partner != num && partnerCount > 0)
+                pairSet.add(new Pair(num, partner));
+        }
+
+//        System.out.println("pairSet = " + pairSet);
+
+        return pairSet.size();
+    }
+
+    private static class Pair {
+        int x;
+        int y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return x == pair.x &&
+                    y == pair.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        @Override
+        public String toString() {
+            final StringBuffer sb = new StringBuffer("Pair{");
+            sb.append("x=").append(x);
+            sb.append(", y=").append(y);
+            sb.append('}');
+            return sb.toString();
+        }
+    }
+}
---
+++ src/test/groovy/com/github/sofiaguyang/leetcode/k_diff_pairs_in_array/KDiffPairsInArrayTest.groovy
@@ -0,0 +1,26 @@
+package com.github.sofiaguyang.leetcode.k_diff_pairs_in_array
+
+import com.google.common.primitives.Ints
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+class KDiffPairsInArrayTest extends Specification {
+
+    @Unroll
+    def "Returns #expected k-diff pairs given nums=#nums and k=#k"() {
+        given:
+        def solution = new KDiffPairsInArray()
+
+        expect:
+        solution.findPairs(Ints.toArray(nums), k) == expected
+
+        where:
+        nums            | k  || expected
+        [3, 1, 4, 1, 5] | 2  || 2
+        [1, 2, 3, 4, 5] | 1  || 4
+        [1, 3, 1, 5, 4] | 0  || 1
+        [1, 2, 3, 4, 5] | -1 || 0
+        [-1, -2]        | 1   | 1
+    }
+}
